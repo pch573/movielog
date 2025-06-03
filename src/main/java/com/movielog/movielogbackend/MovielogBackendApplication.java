5c30e6d5e2ef725efcfbd6cd13373379b75ccfd2
pch573 <cksgho573@gmail.com>
2025-06-03 16:13:47 +0900

    setting commit test

---
+++ src/main/java/com/movielog/movielogbackend/MovielogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.movielog.movielogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovielogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movielog/movielogbackend/MovielogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movielog.movielogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
